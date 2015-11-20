eeaad51e94fcecabeea0fcbd59cd62b6e33450d8
Claudio Viviani <info@homelab.it>
2015-11-20 20:58:49 +0100

    Update wordbrutepress.py

--- wordbrutepress.py
+++ wordbrutepress.py
@@ -24,8 +24,13 @@
 # 2) xml-rpc brute force mode
 # 3) http and https protocols support
 # 4) Random User Agent
+# 5) Big wordlist support
 #
 # CHANGELOG:
+#
+#  2015-11-20 v2.1
+#  1) Add new feature: Big wordlist support (thanks to guly @theguly)
+#  2) Fix faultcode check instead of "403" code for XML-RPC (thanks to guly @theguly)
 #
 #  2015-04-12 v2.0
 #  1) Add new feature xml-rpc brute force mode
@@ -58,7 +63,7 @@ banner = """
        |::.. .  /                    |::.|
        `-------'                     `---'
 
-                                        W0rdBRUTEpr3ss v2.0
+                                        W0rdBRUTEpr3ss v2.1
 
                          Written by:
 
@@ -70,7 +75,6 @@ banner = """
                    homelabit@protonmail.ch
 
         http://ffhd.homelab.it (Free Fuzzy Hashes Database)
-               http://archive-exploit.homelab.it/1
                https://www.facebook.com/homelabit
                  https://twitter.com/homelabit
                https://plus.google.com/+HomelabIt1/
